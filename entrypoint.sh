b51320b4060a37bd5445b5d2748725e22e2d5ea5
Adán Rivas <arivasloureiro@gmail.com>
2024-12-02 16:17:11 +0100

    feat: enable New Relic logging context

--- entrypoint.sh
+++ entrypoint.sh
@@ -29,6 +29,7 @@ then
             -e 's/(newrelic.appname) =.*/\1 = "'$NEW_RELIC_APP_NAME'"/' \
             -e "s/;*(newrelic.error_collector.ignore_exceptions) =.*/\1 = \"$NEW_RELIC_IGNORE\"/" \
             -e 's/;*(newrelic.distributed_tracing_enabled) =.*/\1 = false/' \
+            -e 's/;*(newrelic.application_logging.forwarding.context_data.enabled) =.*/\1 = true/' \
             /usr/local/etc/php/conf.d/newrelic.ini
     fi    
 else
